cad1a2d3724a6e661da25454145ef1b1287b6821
Adi Avraham <adiforipod@gmail.com>
2018-11-29 17:54:00 -0800

    added subreddit command

---
+++ commands/subreddit.js
@@ -0,0 +1,14 @@
+exports.run = (client, message, args) => { 
+    const { Client, RichEmbed } = require('discord.js');
+      const randomPuppy = require('random-puppy');
+      var subreddit = args[0]
+      if (!subreddit) return message.reply(`Captain, that does not compute!`); //shorter plus you had the return on the wrong place
+        randomPuppy(`${subreddit}`)
+                .then(url => {
+                    const embed = new RichEmbed()
+                        .setFooter(`/r/${subreddit}`)
+                        .setImage(url)
+                        .setColor('0xe6e600')
+                    return message.channel.send({ embed })
+                })
+      }
