13818737ccf2547c30c5587f97ee91c170a253d8
Adi Avraham <adiforipod@gmail.com>
2018-12-03 18:09:27 -0800

    making ping embed work

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,8 @@
-exports.run = (client, message, args, func) => {
-    const { Client, RichEmbed } = require('discord.js');
-    const embed = new RichEmbed()
-        .setColor(func.dataHexcode())
-        .addDescription(`Ready captain! ${Math.round(client.ping)} ms`)
-    message.channel.send({embed})
-    console.log(e);
-};
+exports.run = (client, message, func) => {
+        const { Client, RichEmbed } = require('discord.js');
+            const embed = new RichEmbed()
+                .setColor(func.dataHexcode())
+                .addDescription(`Ready captain! ${Math.round(client.ping)} ms`)
+            return message.channel.send({embed}).catch(e);
+    //    message.reply("Ready captain! " + Math.round(client.ping) + "ms").catch(console.error);
+}
