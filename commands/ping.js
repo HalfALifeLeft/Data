212fd41fb3f4a05f4b3bc1788e268adf895454e3
Adi Avraham <adiforipod@gmail.com>
2018-12-03 20:18:26 -0800

    fixing commands

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,8 @@
-exports.run = (client, message/*, func*/) => {
-    //    const { Client, RichEmbed } = require('discord.js');
-    //        const embed = new RichEmbed()
-    //            .setColor(func.dataHexcode())
-    //            .setDescription(`Ready captain! ${Math.round(client.ping)} ms`)
-    //        return message.channel.send({embed})
-        message.reply("Ready captain! " + Math.round(client.ping) + "ms").catch(console.error);
+exports.run = (client, message, func) => {
+        const { Client, RichEmbed } = require('discord.js');
+            const embed = new RichEmbed()
+                .setColor(func.dataHexcode())
+                .setDescription(`Ready captain! ${Math.round(client.ping)} ms`)
+            return message.channel.send({embed})
+//    message.reply("Ready captain! " + Math.round(client.ping) + "ms").catch(console.error);
 };
