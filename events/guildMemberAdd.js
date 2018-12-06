45f840c3a502a83356d906f718ef280ac18e8eef
Adi Avraham <adiforipod@gmail.com>
2018-12-05 20:31:00 -0800

    smol change

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,7 +1,7 @@
 module.exports = (client, member) => {
     // NEVER have client.on inside of a event or command, it FUCKS it up
       console.log(`New User "${member.user.username}" has joined "${member.guild.name}"` );
-      const channel = member.guild.channels.find(ch => ch.name === 'data-testing-general');
+      const channel = member.guild.channels.find(ch => ch.name === 'general');
       // Do nothing if the channel wasn't found on this server
       if (!channel) return;
       // Send the message, mentioning the member
