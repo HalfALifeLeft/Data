2392e0036a0877fd687ef934ab7e497643ebf86a
Adi Avraham <adiforipod@gmail.com>
2018-12-05 19:26:58 -0800

    guildMemberAdd works properly now

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,10 +1,9 @@
 module.exports = (client, member) => {
-    client.on("guildMemberAdd", (member) => {
+    // NEVER have client.on inside of a event or command, it FUCKS it up
       console.log(`New User "${member.user.username}" has joined "${member.guild.name}"` );
       const channel = member.guild.channels.find(ch => ch.name === 'data-testing-general');
       // Do nothing if the channel wasn't found on this server
       if (!channel) return;
       // Send the message, mentioning the member
       channel.send(`Welcome to the server, ${member}`);
-    });
   };
