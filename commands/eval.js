338fd237975ab72f57b958c628025e3e193fafed
Adi Avraham <adiforipod@gmail.com>
2018-12-05 19:17:41 -0800

    Working on GMA

--- commands/eval.js
+++ commands/eval.js
@@ -16,7 +16,7 @@ function clean(text) {
         let evaled = message.content.slice(2 + 4).trim(); 
       // you do .toLowerCase() on args when defining them, so you can't use them in your eval. I made it so it removes the prefix and four letters from the start of the word and then evals everything else
         let evaledCode = eval(evaled);//eval the code
-        message.delete();
+//        message.delete();
         const embed = new RichEmbed()
         .setAuthor(message.author.username + "#" + message.author.discriminator, message.author.avatarURL)
         .setThumbnail(client.user.avatarURL)
@@ -25,7 +25,7 @@ function clean(text) {
         .addField("Output 📤 ", `\`\`\`${evaledCode}\`\`\``)
           message.channel.send(embed)
       } catch (err) {
-        message.delete();
+//        message.delete();
         let evaled = message.content.slice(1 + 4).trim();
         const embed = new RichEmbed()
         .setAuthor(message.author.username + "#" + message.author.discriminator, message.author.avatarURL)
--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,7 +1,7 @@
 module.exports = (client, member) => {
     client.on("guildMemberAdd", (member) => {
       console.log(`New User "${member.user.username}" has joined "${member.guild.name}"` );
-      const channel = member.guild.channels.find(ch => ch.name === 'general');
+      const channel = member.guild.channels.find(ch => ch.name === 'data-testing-general');
       // Do nothing if the channel wasn't found on this server
       if (!channel) return;
       // Send the message, mentioning the member
