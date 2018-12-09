7df21608a874a9b0938cb54a410df505a13a3536
Adi Avraham <adiforipod@gmail.com>
2018-12-09 01:49:35 -0800

    .env fix

--- events/message.js
+++ events/message.js
@@ -2,7 +2,7 @@ module.exports = (client, message) => {
     // Ignore all bots
     if (message.author.bot) return;
     // Ignore messages not starting with the prefix (in config.json)
-    if (message.content.indexOf(client.config.prefix) !== 0) return;
+    if (message.content.indexOf(process.env.PREFIX) !== 0) return;
     // Our standard argument/command name definition.
     const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
