f0943f59d2bc45ac1c06ceaded16b340c24bcc56
Half <adiforipod@gmail.com>
2019-01-03 16:08:17 -0800

    no longer logs DMs

--- events/channelCreate.js
+++ events/channelCreate.js
@@ -2,10 +2,10 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
 module.exports = (client, Channel) => {
-//	if (client.channels.type("dm")) return;
 	// NEVER have client.on inside of a event or command, it FUCKS it up
-	const mychannel = client.channels.find(channel => channel.name === "server-events")
+	const mychannel = client.channels.find(channel => channel.name === "server-events");
 	if (!mychannel) return;
+	if (Channel.type == "dm") return;
 	console.log(`${Channel} was created` );
 	const { Client, RichEmbed } = require('discord.js');
 	const embed = new RichEmbed()
