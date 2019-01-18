3f1f4d0a65aa4e651f8a09621898f00a1afa95bb
Half <adiforipod@gmail.com>
2019-01-18 00:59:19 -0800

    help changes/syntax command changes

--- commands/embed.js
+++ commands/embed.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 exports.run = (client, message, args ) => {
-    //Check if the bot owner is the author
-	if (client.id !== client.ownerID) return message.channel.send('**You lack the required permissions to use this command.**');
+	let allowedRole = message.guild.roles.find(`name`, `Admins`);
+	if (message.member.roles.has(allowedRole.id)) return message.channel.send(`**You lack the required permissions to use this command.**`);
 	//if owner isnt the one sending the message it replies with no perms
 	let color = args[0];
-	let content = args.slice(1).join(" ");
+	let content = args.slice(1).join(` `);
 	if (content.length > 1024) return message.channel.send(`Your embed exceeds the character limit of 1024`);
-	const { Client, RichEmbed } = require('discord.js');
+	const { Client, RichEmbed } = require(`discord.js`);
 	const embed = new RichEmbed()	
 		.setColor(`${color}`)
-		.setDescription(`${content}`)
+		.setDescription(`${content}`);
 	return message.channel.send({embed});
 };
--- commands/subreddit.js
+++ commands/subreddit.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 exports.run = (client, message, args,  func) => { 
-	const { Client, RichEmbed } = require('discord.js');
-	const randomPuppy = require('random-puppy');
+	const { Client, RichEmbed } = require(`discord.js`);
+	const randomPuppy = require(`random-puppy`);
 	var subreddit = args[0];
-	if (!subreddit) return message.reply('Captain, that does not compute!'); //shorter plus you had the return on the wrong place
+	if (!subreddit) return message.reply(`Captain, that does not compute!`); //shorter plus you had the return on the wrong place
 	randomPuppy(`${subreddit}`)
 		.then(url => {
 			const embed = new RichEmbed()
