a016fd63d45822ee88cb7b5fd385eae840af49d8
Half <adiforipod@gmail.com>
2019-02-07 20:57:00 -0800

    userinfo fully works!

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -6,41 +6,26 @@ exports.run = (client, message, args) => {
     var moment = require(`moment`);
     let members = [];
     let indexes = [];
-    let userFromId = ``;
-    const regExTest = RegExp(/<@\d+/);
-
-    client.fetchUser(args[0]);
+    let argumentZero = args[0];
+    var regExTest = RegExp(/<@!?\d+>/);
 
     if(!args[0]) {
-        args[0] = message.author.username;
-    } else {
-        console.log(`RegEx Outer Else Worked`);
-        if(args[0] === regExTest) {
-            console.log(`RegEx worked`);
-            //async () => {
-            /*   try {
-                    const User = await client.fetchUser(id) 
-                    const member = await guild.fetchmember(User)
-                    const role = guild.roles.find(r => r.name === `Testing`);
-                    await member.addRole(role);
-                    await channel.send(`Success!`);
-                } catch (e) {
-                    console.error(e);
-                } */
-            //}
-        }
-    }
+        argumentZero = message.author.username;
+    } 
+
     message.guild.members.forEach(function(member){
         members.push(member.user.username);
         indexes.push(member.id);
     });
-    let match = sm.findBestMatch(args[0], members); //matching first argument with a name from the members array
+
+    let match = sm.findBestMatch(argumentZero, members); //matching first argument with a name from the members array
     let username = match.bestMatch.target; //get the best match from variable match
     let memberIndex = message.guild.members.get(indexes[members.indexOf(username)]); //want the ID of the matched user, so we take the position of the name from members array and match it with the ID in the position of the indexes array.
     let usernameUser = ``;
     let userMention = ``;
-    if(args[0] === regExTest) {
-        console.log(`RegExp Worked`);
+
+    if(regExTest.test(argumentZero) === true) {
+        memberIndex = message.guild.members.get(args[0].replace(`<@`, ``).replace(`!`, ``).replace(`>`, ``));
     }
 
     if (message.mentions.everyone === true) return message.reply(`You cannot tag everyone with this command!`);
@@ -54,15 +39,18 @@ exports.run = (client, message, args) => {
     }
     let userOptions = usernameUser || userMention; //get the options for how a user is selected
     let user = userOptions; //get the option that is used by the user
-
+    let a = moment(Date.now());
+    let b = moment(user.createdTimestamp);
+    let c = moment(memberIndex.joinedTimestamp);
+    
     const { Client, RichEmbed } = require(`discord.js`);
     const embed = new RichEmbed()
         .setTitle(`User Info: ${user.username}#${user.discriminator}`)
         .setDescription(`Chilling in ${user.presence.status} status`)
         .setColor(process.env.HEXCODE)
-        .addField(`Created:`,`${moment.utc(user.createdTimestamp).format(`MM/DD/YYYY - hh:mm:ss`)}`, true)
-        .addField(`Joined Server:`,`${moment.utc(memberIndex.joinedTimestamp).format(`MM/DD/YYYY - hh:mm:ss`)}`)
-        .addField(`Roles:`,`${memberIndex.roles.filter(r => r.name !== `@everyone`).map(r => `${r.name}`).join(`, `) || `No Roles`}`)
+        .addField(`Created:`,`${moment.utc(user.createdTimestamp).format(`MM/DD/YYYY - hh:mm:ss`)} (${a.diff(b, `days`)} days ago!)`, true)
+        .addField(`Joined Server:`,`${moment.utc(memberIndex.joinedTimestamp).format(`MM/DD/YYYY - hh:mm:ss`)} (${a.diff(c, `days`)} days ago!)`)
+        .addField(`Roles:`,`${memberIndex.roles.filter(r => r.name !== `@everyone`).map(r => `<@&${r.id}>`).join(`, `) || `No Roles`}`)
         .setThumbnail(user.avatarURL)
         .setFooter(`User ID: ${user.id}`);
     message.channel.send({embed});
