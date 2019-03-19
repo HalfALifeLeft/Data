7f603d8c5cc2722b60c1a7acb66dea693a1d9932
Half <adiforipod@gmail.com>
2019-03-18 18:07:45 -0700

    perms based, and event now logs deleted messages

--- events/messageDeleteBulk.js
+++ events/messageDeleteBulk.js
@@ -1,8 +1,11 @@
+/* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
-module.exports = (client, messages) => {
-    const channelId = client.dataConfig.get(`${message.guild.id}`, `messageLogs`);
-    if (message.guild.channels.has(channelId) == false) return;
+module.exports = async (client, messages) => {
+    var fs = require(`fs`);
+    var moment = require(`moment`);
+    const channelId = client.dataConfig.get(`${messages.first().guild.id}`, `messageLogs`);
+    if (messages.first().guild.channels.has(channelId) == false) return;
 
     const mychannel = messages.first().guild.channels.find(channel => channel.id === channelId);
     if (!mychannel) return;
@@ -17,4 +20,29 @@ module.exports = (client, messages) => {
         .addField(`Messages Deleted`, `${messages.array().length} messages deleted!`)
         .setFooter(`#${messages.first().channel.name}`);
     mychannel.send(embed);
+
+    await fs.writeFile(`messageDeleteBulk.txt`, `Messages deleted on ${moment.utc(Date.now()).format(`MMMM DD, YYYY, hh:mm:ss a`)}` + `\r\n`, {
+        'flags': `a`
+    }, function (err, data) {
+        if (err) console.error(err);
+    });
+
+    messages.tap(msg => {
+        fs.appendFile(`messageDeleteBulk.txt`, `${msg.author.username}#${msg.author.discriminator} (${msg.author.id}) at ${moment.utc(msg.createdTimestamp).format(`MMMM DD, YYYY, hh:mm:ss a`)} message ID: ${msg.id}: \n` + msg.content + `\r\n`, {
+            'flags': `a`
+        }, function (err) {
+            if (err) console.error(err);
+        });
+
+    });
+
+    await mychannel.send({files: [{
+        attachment: `./messageDeleteBulk.txt`,
+        name: `messageDeleteBulk.txt`
+    }]
+    });
+
+    fs.unlink(`./messageDeleteBulk.txt`, (e) => {
+        if (e) return console.error(e);
+    });
 };
