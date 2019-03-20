7fa18060495d3ab85410ffb696b3f11f42c5ca6b
Half <adiforipod@gmail.com>
2019-03-20 15:00:36 -0700

    reorganized guildMemberAdd

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,18 +2,14 @@
 /* eslint-disable no-console */
 /* eslint-disable no-undef */
 module.exports = (client, member) => {
-    const channelId = client.dataConfig.get(`${member.guild.id}`, `memberLogs`);
-    const generalId = client.dataConfig.get(`${member.guild.id}`, `welcomeChannel`);
-    if (member.guild.channels.has(channelId) == false) return;
-    if (member.guild.channels.has(generalId) == false) return;
-
-    // NEVER have client.on inside of a event or command, it FUCKS it up
-    const mychannel = member.guild.channels.find(channel => channel.id === channelId);
-    const general = member.guild.channels.find(channel => channel.id === generalId);
     var role = member.guild.roles.find(role => role.name === `Members`);
-
     if (role == null) return;
     member.addRole(role);
+
+    const channelId = client.dataConfig.get(`${member.guild.id}`, `memberLogs`);
+    if (member.guild.channels.has(channelId) == false) return;
+
+    const mychannel = member.guild.channels.find(channel => channel.id === channelId);
 
     if (mychannel == null) return;
     const {
@@ -25,6 +21,11 @@ module.exports = (client, member) => {
         .setColor(process.env.GOOD)
         .addField(`Member joined`, `${member.user.username}#${member.user.discriminator} (${member.user.id})`);
     mychannel.send(embed);
+
+    const generalId = client.dataConfig.get(`${member.guild.id}`, `welcomeChannel`);
+    if (member.guild.channels.has(generalId) == false) return;
+
+    const general = member.guild.channels.find(channel => channel.id === generalId);
 
     if (general == null) return;
     general.send(`<@${member.id}> has joined the server!`);
