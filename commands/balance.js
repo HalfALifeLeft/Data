049a8ad1415717e11d910a2428d910481c8ce8a0
Glitch (data-discord-bot) <none>
2019-03-21 15:30:55 +0000

    balance fix, ensure added/works with @user

--- commands/balance.js
+++ commands/balance.js
@@ -2,13 +2,21 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 module.exports.run = async (client, message, args) => {
-    let userName = args[0];
-    let userArgs = args.slice(1).join(` `);
-    let regExTestOne = RegExp(/<#!?\d+>/);
-    let regExTestTwo = RegExp(/<@!?\d+>/);
-    const key = `${message.member.user.id}-${message.guild.id}`;
+    let user = message.author.id;
+    if(!message.mentions) {
+       user = message.mentions.members.first().id;
+    }
+    const key = `${user}-${message.guild.id}`;
     
-
+    client.currency.ensure(key, {
+        userID: user,
+        guildID: message.guild.id,
+        points: 0,
+        level: 1,
+        lastSeen: new Date()
+    });
+    //return message.reply(`pls wait 4 Half 2 fix :)`);
+  
     message.channel.send(`You currently have ${client.currency.get(key, `points`)} points, and are level ${client.currency.get(key, `level`)}!`);
 
 
