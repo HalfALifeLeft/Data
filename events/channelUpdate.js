3dce013f703a5b2f445d88ea9fc68f88608c9be5
Adi Avraham <adiforipod@gmail.com>
2019-03-21 19:58:23 -0700

    channelPinsUpdate remove, channelUpdate v1

--- events/channelUpdate.js
+++ events/channelUpdate.js
@@ -2,5 +2,35 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
 module.exports = async (client, oldChannel, newChannel) => {
+    const channelId = client.dataConfig.get(`${oldChannel.guild.id}`, `serverLogs`);
+    let topicOld = oldChannel.topic;
+    let topicNew = newChannel.topic;
+    if (oldChannel.guild.channels.has(channelId) == false) return;
 
+    const mychannel = oldChannel.guild.channels.find(channel => channel.id === channelId);
+    if (!mychannel) return;
+
+    if(oldChannel == newChannel) return;
+
+    if(oldChannel.name == newChannel.name && oldChannel.parentID == newChannel.parentID && oldChannel.permissionOverwrites == newChannel.permissionOverwrites && oldChannel.topic == newChannel.topic && oldChannel.nsfw == newChannel.nsfw) return;
+
+    if(topicOld == ``) {
+        topicOld = `Nothing`;
+    }
+
+    if(topicNew == ``) {
+        topicNew = `Nothing`;
+    }
+
+    const {
+        Client,
+        RichEmbed
+    } = require(`discord.js`);
+    const embed = new RichEmbed()
+        .setTimestamp()
+        .setColor(process.env.GOOD)
+        .addField(`Channel Updated`, `<#${oldChannel.id}> (\`${oldChannel.id}\`)`)
+        .addField(`Old Channel Details`, `Name: ${oldChannel.name}\nCategory: ${oldChannel.parent.name}\nChannel Topic: ${topicOld}\nNSFW: ${oldChannel.nsfw}`)
+        .addField(`Old Channel Details`, `Name: ${newChannel.name}\nCategory: ${newChannel.parent.name}\nChannel Topic: ${topicNew}\nNSFW: ${newChannel.nsfw}`);
+    mychannel.send(embed);
 };
