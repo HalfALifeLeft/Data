58c5feb5fe621ef061b03b62fcea8e4488e03715
Glitch (data-discord-bot) <none>
2019-03-25 15:40:54 +0000

    wording fix

--- commands/embed.js
+++ commands/embed.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 module.exports.run = async (client, message, args) => {
-    if (message.member.hasPermission(`ADMINISTRATOR`, false, true, true)) return message.channel.send(`**You need the \`ADMINISTRATOR\` permission to use this command.**`);
+    if (message.member.hasPermission(`ADMINISTRATOR`, false, true, true)) return message.channel.send(`**You do not have the \`ADMINISTRATOR\` permission.**`);
     //if owner isnt the one sending the message it replies with no perms
     let color = args[0];
     let content = args.slice(1).join(` `);
