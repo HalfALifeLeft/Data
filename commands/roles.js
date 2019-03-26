60a85844cfea34266ec7cb9b9b4530e00a54273b
Half <adiforipod@gmail.com>
2019-03-25 23:00:58 -0700

    fixed roles, now need redesign

--- commands/roles.js
+++ commands/roles.js
@@ -17,18 +17,21 @@ module.exports.run = async (client, message, args) => {
         .then((msg) => {
             msg.react(`⬅`)
                 .then(() => {
-                    msg.react(`➡`)
-                        .catch((e) => {
-                            console.error(e);
-                        });
+                    setTimeout(function () {
+                        msg.react(`➡`)
+                            .catch((e) => {
+                                console.error(e);
+                            });
+                    }, 250);
                 })
                 .catch((e) => {
                     console.error(e);
                 });
 
             const collector = msg.createReactionCollector((reaction, user) =>
-                user.id === message.author.id &&
+                user.id === message.author.id && //NEEDS TO VERIFY MESSAGE AUTHOR TWICE, ITS LIKE (A & B) or (C & D) NOT A & (B OR C).
                 reaction.emoji.name === `⬅` ||
+                user.id === message.author.id &&
                 reaction.emoji.name === `➡`, {
                     time: 300000
                 }
@@ -37,19 +40,7 @@ module.exports.run = async (client, message, args) => {
             collector.on(`collect`, reaction => {
                 const react = reaction.emoji.name;
 
-                if (react === `⬅`) {
-                    i--;
-                    o--;
-                    if (i < 0) {
-                        i = Math.ceil(allRoles.length / 20) - 1;
-                    }
-                    if (o < 1) {
-                        o = Math.ceil(allRoles.length / 20);
-                    }
-                    tenRoles = allRoles.slice(i * 20, o * 20);
-                    msg.edit(`\`\`\`**Page (${o}/${Math.ceil(allRoles.length / 20)})**\n` + tenRoles.join(`\n`).toString() + `\`\`\``);
-                    reaction.remove(message.author);
-                } else if (react === `➡`) {
+                if (react === `➡`) {
                     i++;
                     o++;
                     if (i >= Math.ceil(allRoles.length / 20)) {
@@ -60,7 +51,23 @@ module.exports.run = async (client, message, args) => {
                     }
                     tenRoles = allRoles.slice(i * 20, o * 20);
                     msg.edit(`\`\`\`**Page (${o}/${Math.ceil(allRoles.length / 20)})**\n` + tenRoles.join(`\n`).toString() + `\`\`\``);
-                    reaction.remove(message.author);
+                    setTimeout(function () {
+                        reaction.remove(message.author.id);
+                    }, 250);
+                } else if (react === `⬅`) {
+                    i--;
+                    o--;
+                    if (i < 0) {
+                        i = Math.ceil(allRoles.length / 20) - 1;
+                    }
+                    if (o < 1) {
+                        o = Math.ceil(allRoles.length / 20);
+                    }
+                    tenRoles = allRoles.slice(i * 20, o * 20);
+                    msg.edit(`\`\`\`**Page (${o}/${Math.ceil(allRoles.length / 20)})**\n` + tenRoles.join(`\n`).toString() + `\`\`\``);
+                    setTimeout(function () {
+                        reaction.remove(message.author.id);
+                    }, 250);
                 }
 
                 setTimeout(function () {
