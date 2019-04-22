65ad30afb0ae33254b83bfb5397a0b9543bddc9c
Adi Avraham <30277165+HalfALifeLeft@users.noreply.github.com>
2019-04-21 20:00:01 -0700

    added permissions for channel/invite

--- commands/invite.js
+++ commands/invite.js
@@ -2,6 +2,11 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 module.exports.run = async (client, message, args) => {
+
+    if (!message.member.hasPermission(`CREATE_INSTANT_INVITE`, false, true, true)) {
+        return message.reply(`You don't have the \`CREATE_INSTANT_INVITE\` permission!`);
+    }
+
     message.channel.createInvite()
         .then(invite => {
             message.author.send(invite.url);
