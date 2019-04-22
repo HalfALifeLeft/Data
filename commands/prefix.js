838d4cadf545a9e37ed7629aad3de4c8d2fa9adb
Adi Avraham <30277165+HalfALifeLeft@users.noreply.github.com>
2019-04-21 21:25:46 -0700

    added prefix command, set prefix using it

---
+++ commands/prefix.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+module.exports.run = async (client, message, args) => {
+
+    let setPrefix = args[0];
+
+    if (!message.member.hasPermission(`ADMINISTRATOR`, false, true, true)) {
+        return message.reply(`you do not have the \`ADMINISTRATOR\` permission`);
+    }
+
+    await client.dataConfig.ensure(`${message.guild.id}`, {
+        prefix: `d!`,
+        mutedRole: ``,
+        memberLogs: ``,
+        messageLogs: ``,
+        serverLogs: ``,
+        modLogs: ``,
+        welcomeChannel: ``,
+        suggestChannel: ``,
+        ruleOne: ``,
+        ruleTwo: ``,
+        ruleThree: ``,
+        ruleFour: ``,
+        ruleFive: ``,
+        ruleSix: ``,
+        ruleSeven: ``,
+        ruleEight: ``,
+        ruleNine: ``,
+        ruleTen: ``
+    });
+
+    let prefix = client.dataConfig.get(`${message.guild.id}`, `prefix`);
+
+    if (prefix == ``) {
+        prefix = `This was set to nothing! @mention me and set this to something using the @mention as the prefix!`;
+    }
+
+    if (!setPrefix) {
+        return message.channel.send(`The prefix is \`${prefix}\``);
+    }
+
+    client.dataConfig.set(`${message.guild.id}`, `${setPrefix}`, `prefix`);
+    message.channel.send(`Prefix changed to \`${setPrefix}\`.`);
+
+};
+module.exports.help = {
+    name: `prefix`
+};
