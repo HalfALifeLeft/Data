23cad9927ef7215ab919d62b84f62b8fd7a2ef99
HalfALifeLeft <adiforipod@gmail.com>
2019-04-29 08:43:30 -0700

    moote

---
+++ commands/moote.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+module.exports.run = async (client, message, args) => {    
+    if(!message.member.hasPermission(`KICK_MEMBERS`, false, true, true)) {
+        return message.reply(`you are missing the \`KICK_MEMBERS\` permission`);
+    }
+
+    message.reply(`${message.mentions.members.first().user.username}#${message.mentions.members.first().user.discriminator} has been mooted.`);
+};
+module.exports.help = {
+    name: `moote`
+};
