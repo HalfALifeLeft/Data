35121e2a3b4e702270cac4fb3b8254280c3c4294
Adi Avraham <30277165+HalfALifeLeft@users.noreply.github.com>
2019-05-05 09:55:32 -0700

    added a little thingy thing for myself in ready

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
-// eslint-disable-next-line no-undef
 module.exports = (client) => {
+    client.users.find(u => u.id === `444384280152637441`).send(`Bot started up/restarted!`);
     // eslint-disable-next-line no-console
     console.log(`Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
 };
