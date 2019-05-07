ffd1e7724696c98e92d1f19a1643916f37aa995d
HalfALifeLeft <adiforipod@gmail.com>
2019-05-07 11:00:30 -0700

    added status

--- events/ready.js
+++ events/ready.js
@@ -2,4 +2,13 @@ module.exports = (client) => {
     client.users.find(u => u.id === `444384280152637441`).send(`Bot started up/restarted!`);
     // eslint-disable-next-line no-console
     console.log(`Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
+
+    client.user.setPresence({
+            game: {
+                name: `on the USS Enterprise`
+            },
+            status: `online`
+        })
+        // eslint-disable-next-line no-console
+        .catch(console.error);
 };
