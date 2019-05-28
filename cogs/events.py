dc6b071d2420eaf39e304a5d54f1ae8cbcb9f0c9
HalfALifeLeft <adiforipod@gmail.com>
2019-05-28 08:20:04 -0700

    adding everything back!

---
+++ cogs/events.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands, tasks
+from itertools import cycle
+
+#status = cycle(['Status 1', 'Status 2'])
+
+class Events(commands.Cog):
+    
+    def __init__(self, client):
+        self.client = client
+                      
+    #events 
+    
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.change_status.start()
+        print('Bot is ready!')
+    
+    @tasks.loop(hours=1)
+    async def change_status(self):
+        print('Guess I started the loop!')
+        #await self.client.change_presence(activity=discord.Game(next(status)))
+        await self.client.change_presence(activity=discord.Game(f'in {len(self.client.guilds)} Servers for {len(self.client.users)} Users!'))
+        print(f'Working in {len(self.client.guilds)} Servers for {len(self.client.users)} Users!')
+    
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        print(f'{member} has joined a server.')
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        print(f'{member} has been removed from a server.')
+        
+def setup(client):
+    client.add_cog(Events(client))
---
+++ server.py
@@ -0,0 +1,36 @@
+import discord
+import os
+import logging
+from dotenv import load_dotenv
+from discord.ext import commands, tasks
+from itertools import cycle
+load_dotenv()
+
+client = commands.Bot(command_prefix = '!')
+
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+    
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    
+@client.command()
+async def reload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
+    
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+    
+client.run(os.getenv("TOKEN"))
+
+#nodemon --exec py -3 server.py
